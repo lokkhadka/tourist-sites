6d55e07019b704d7041de0b029bf211a7d1710b8
lokkhadka <lkhadka60@gmail.com>
2018-03-17 12:11:56 -0400

    slideshow and flying text has been added

--- js/photojquery.js
+++ js/photojquery.js
@@ -6,3 +6,25 @@ $(function() {
 }) ();
 
 
+
+
+$(document).ready(function(){
+   $('#divContainer img').on({
+       mouseover : function(){
+           $(this).css({
+               'curser': 'pointer',
+               'border-color': 'red'
+           });
+       },
+       mouseout : function(){
+        $(this).css({
+            'curser': 'default',
+            'border-color': 'grey'
+        });  
+       },
+       click : function(){
+           var imageUrl = $(this).attr('src');
+           $('#mainImg').attr('src', imageUrl);
+       }
+   });
+});
